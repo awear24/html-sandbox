b2c03bbf039ec9e6304d97bf97f87ff85242fa7d
Augustina Wear <awear@cnm.edu>
2017-07-12 14:31:37 -0600

    added image

--- index.php
+++ index.php
@@ -5,6 +5,60 @@
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>This is my web page</h1>
+		<h1>This is my web page.</h1>
+
+		<table>
+			<tr>
+			<th>Cats</th>
+			<th>Dogs</th>
+			</tr>
+				<tr>
+					<td>Persian</td>
+					<td>Begale</td>
+				</tr>
+				<tr>
+					<td>Maine Coon</td>
+					<td>Rottweiler</td>
+				</tr>
+			<tr>
+				<td>Siamese</td>
+				<td>German Shepard</td>
+			</tr>
+		</table>
+
+		<!--This is an HTML comment-->
+		<h2>A Few of My favorite Things</h2>
+		<ul>
+		<li>Tacos<li>
+		<li>The Beach</li>
+		<li>Kittens</li>
+			<li>Fast Cars</li>
+		</ul>
+
+		<h2>Things to Do Today</h2>
+		<ol>
+			<li>Feed the Kitty</li>
+			<li>Give Dylan a hard time</li>
+			<li>Water the plants</li>
+			<li>Hack the vulnerabilities in bootcamp-coders.</li>
+		</ol
+
+		<h2>I <3 Pikachu</h2>
+		<img src="images/016_16.JPG" alt="016_16"/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	</body>
 </html>
